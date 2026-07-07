f2d50e68c16aa244b4491d452ec4c18593b1ef65
J-bit-A <josueahyundai@gmail.com>
2026-07-06 19:38:51 -0600

    c

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-
 router.post('/registrar', authController.registrar);
 router.post('/login', authController.login);
 router.post('/google', authController.googleLogin); 
 
-
 router.post('/forgot-password', authController.solicitarRecuperacion);
 router.post('/reset-password', authController.restablecerContrasena);
+
+router.get('/verificar-proveedor', authController.verificarProveedor);
 
 module.exports = router;
