9f0674c92338c0c1ead7b6de72631d00f6e743e5
J-bit-A <josueahyundai@gmail.com>
2026-07-07 13:34:38 -0600

    parce

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,14 +3,17 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { OAuth2Client } = require('google-auth-library');
 const nodemailer = require('nodemailer'); 
-
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS  
+    },
+    tls: {
+        rejectUnauthorized: false 
     }
 });
 
