75c60a50da103aad8a0a4159112b6f2d3f0bea0c
Aldox00 <251213@ids.upchiapas.edu.mx>
2026-07-22 09:35:12 -0600

    Fix products endpoint

--- src/models/productoModel.js
+++ src/models/productoModel.js
@@ -1,8 +1,8 @@
 const pool = require('../config/db');
 
 const Producto = {
-    create: async (productoData) => {
-        const query = 'INSERT INTO productos (usuario_id, nombre, precio) VALUES (?, ?, ?)';
+    create: async(productoData) => {
+        const query = 'INSERT INTO productos (usuario_id, nombre, precio_venta) VALUES (?, ?, ?)';
         const [result] = await pool.query(query, [
             productoData.usuario_id,
             productoData.nombre,
@@ -11,19 +11,19 @@ const Producto = {
         return result;
     },
 
-    findActivosByUsuarioId: async (usuario_id) => {
+    findActivosByUsuarioId: async(usuario_id) => {
         const query = 'SELECT * FROM productos WHERE usuario_id = ? AND activo = 1';
         const [rows] = await pool.query(query, [usuario_id]);
         return rows;
     },
 
-    findAllByUsuarioId: async (usuario_id) => {
+    findAllByUsuarioId: async(usuario_id) => {
         const query = 'SELECT * FROM productos WHERE usuario_id = ?';
         const [rows] = await pool.query(query, [usuario_id]);
         return rows;
     },
 
-    updateEstado: async (id, activo) => {
+    updateEstado: async(id, activo) => {
         const query = 'UPDATE productos SET activo = ? WHERE id = ?';
         const [result] = await pool.query(query, [activo, id]);
         return result;
