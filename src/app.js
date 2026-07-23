9174a15e4de460128a9e0f1d29a055e36004ae30
Aldox00 <251213@ids.upchiapas.edu.mx>
2026-07-23 00:46:24 -0600

    Fix: completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cors = require('cors');  
+const cors = require('cors');
 const pool = require('./config/db');
-const authRoutes = require('./routes/authRoutes'); 
+const authRoutes = require('./routes/authRoutes');
 const jornadaRoutes = require('./routes/jornadaRoutes');
 const productoRoutes = require('./routes/productoRoutes');
 const ventaRoutes = require('./routes/ventaRoutes');
@@ -14,16 +14,15 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
-    res.json({ 
+    res.json({
         estado: "servidor corriendo",
         proyecto: "FlowPay",
         fecha_servidor: new Date(),
-        cesar: "alejandro",
-        aldo: "puto"
+        cesar: "alejandro"
     });
 });
 
-app.use('/api/auth', authRoutes); 
+app.use('/api/auth', authRoutes);
 app.use('/api/jornada', jornadaRoutes);
 app.use('/api/producto', productoRoutes);
 app.use('/api/venta', ventaRoutes);
--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const productoController = require('../controllers/productoController');
 
-router.post('/crear', productoController.crearProducto);// Solo los activos para vender
-router.get('/catalogo/:usuario_id', productoController.obtenerTodoElCatalogo); 
-router.put('/actualizar-estado', productoController.actualizarEstadoProducto); 
+router.post('/crear', productoController.crearProducto);
+router.get('/activos/:usuario_id', productoController.obtenerProductosActivos);
+router.get('/catalogo/:usuario_id', productoController.obtenerTodoElCatalogo);
+router.put('/actualizar-estado', productoController.actualizarEstadoProducto);
 
 module.exports = router;
