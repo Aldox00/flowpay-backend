eeaa7015b0eca34b19e805097f268305c1e97f47
Alejandro010825 <251219@ids.upchiapas.edu.mx>
2026-07-28 09:53:14 -0600

    mas cosas equis

--- src/models/encuestaModel.js
+++ src/models/encuestaModel.js
@@ -1,16 +1,25 @@
 const pool = require('../config/db');
 
 const Encuesta = {
-    guardarEncuesta: async (jornada_id, id_usuario, pregunta_1, pregunta_2, pregunta_3) => {
+    guardarEncuesta: async (id_usuario, pregunta_1, pregunta_2, pregunta_3) => {
         const connection = await pool.getConnection();
         try {
             await connection.beginTransaction();
 
-            const queryEncuesta = 'INSERT INTO encuestas (jornada_id, id_usuario, pregunta_1, pregunta_2, pregunta_3) VALUES (?, ?, ?, ?, ?)';
-            await connection.query(queryEncuesta, [jornada_id, id_usuario, pregunta_1, pregunta_2, pregunta_3]);
+            const queryEncuesta = 'INSERT INTO encuestas (id_usuario, pregunta_1, pregunta_2, pregunta_3) VALUES (?, ?, ?, ?)';
+            await connection.query(queryEncuesta, [id_usuario, pregunta_1, pregunta_2, pregunta_3]);
 
-            const queryActualizarJornada = 'UPDATE jornadas SET encuesta_contestada = 1 WHERE id = ?';
-            await connection.query(queryActualizarJornada, [jornada_id]);
+            const queryActualizarJornada = `
+                UPDATE jornadas j
+                JOIN (
+                    SELECT id FROM jornadas
+                    WHERE usuario_id = ? AND estado = 'cerrada'
+                    ORDER BY fecha_fin DESC
+                    LIMIT 1
+                ) ultima ON j.id = ultima.id
+                SET j.encuesta_contestada = 1
+            `;
+            await connection.query(queryActualizarJornada, [id_usuario]);
 
             await connection.commit();
             return true;
--- src/services/encuestaService.js
+++ src/services/encuestaService.js
@@ -1,7 +1,7 @@
 const Encuesta = require('../models/encuestaModel');
 
-exports.registrarEncuestaService = async (jornada_id, id_usuario, pregunta_1, pregunta_2, pregunta_3) => {
-    await Encuesta.guardarEncuesta(jornada_id, id_usuario, pregunta_1, pregunta_2, pregunta_3);
+exports.registrarEncuestaService = async (id_usuario, pregunta_1, pregunta_2, pregunta_3) => {
+    await Encuesta.guardarEncuesta(id_usuario, pregunta_1, pregunta_2, pregunta_3);
     
     return {
         msg: '¡Encuesta guardada con éxito!'
